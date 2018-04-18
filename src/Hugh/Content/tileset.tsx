9b6c69cde88ae1f6a976eacf1b0b616bd8402e49
Elijah Mirecki <elimirks@gmail.com>
2018-04-18 23:37:39 +0200

    Added basic prototype assets

---
+++ src/Hugh/Content/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="6" columns="2">
+ <image source="src/Hugh/Content/tilemap.png" trans="ffffff" width="64" height="96"/>
+</tileset>
