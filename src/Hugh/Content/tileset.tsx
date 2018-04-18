2791f0db18ab168f8c18210ebb5c2dc70c3a3169
Elijah Mirecki <elimirks@gmail.com>
2018-04-19 00:01:27 +0200

    Added first prototype level

--- src/Hugh/Content/tileset.tsx
+++ src/Hugh/Content/tileset.tsx
@@ -1,4 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="tileset" tilewidth="32" tileheight="32" tilecount="6" columns="2">
- <image source="src/Hugh/Content/tilemap.png" trans="ffffff" width="64" height="96"/>
+ <image source="tilemap.png" trans="ffffff" width="64" height="96"/>
+ <tile id="2" type="ground"/>
+ <tile id="5" type="player"/>
 </tileset>
