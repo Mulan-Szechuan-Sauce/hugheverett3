2aff5e56b037b9b81fa8a2fbeeb931cf2dd0c0c6
Elijah Mirecki <elimirks@gmail.com>
2018-04-25 12:19:29 +0200

    Some small level design updates

--- src/Hugh/Content/tileset.tsx
+++ src/Hugh/Content/tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilemap" tilewidth="32" tileheight="32" tilecount="6" columns="2">
- <image source="tilemap.png" trans="ffffff" width="64" height="96"/>
+<tileset name="tilemap" tilewidth="32" tileheight="32" tilecount="12" columns="2">
+ <image source="tilemap.png" trans="ffffff" width="64" height="192"/>
  <tile id="0">
   <properties>
    <property name="type" value="death"/>
@@ -24,6 +24,36 @@
  <tile id="5">
   <properties>
    <property name="type" value="player"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="type" value="flatground"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="type" value="flatground"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="type" value="flatground"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="type" value="flatground"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="type" value="flatground"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="type" value="flatground"/>
   </properties>
  </tile>
 </tileset>
