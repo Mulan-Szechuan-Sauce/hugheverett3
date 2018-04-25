2d7cff6c8dc4ad4203923180844bba4c81092881
Josh Wolfe <joshuafwolfe@gmail.com>
2018-04-24 20:22:55 -0400

    This works

---
+++ src/Hugh/Enums/TileType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hugh.Enums
+{
+    public enum TileType
+    {
+        Empty,
+        FlatGround,
+        Player,
+        Finish,
+        Death
+    }
+}
