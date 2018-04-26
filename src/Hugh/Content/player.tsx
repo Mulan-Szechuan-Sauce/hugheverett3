e34fbfcea9fd19f3ab5c6d43c99389f03b1362de
Elijah Mirecki <elimirks@gmail.com>
2018-04-26 12:36:17 +0200

    Ref #10. Created a new player tileset
    With b34ut1ful gr4ph1cs

---
+++ src/Hugh/Content/player.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="player" tilewidth="32" tileheight="48" tilecount="1" columns="1">
+ <image source="player.png" width="32" height="48"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="player"/>
+  </properties>
+ </tile>
+</tileset>
