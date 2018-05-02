e247ba34460e89300f55b1c1b4400979461ab2cb
Elijah Mirecki <elimirks@gmail.com>
2018-05-02 09:11:38 +0200

    Fix #14. Add a player texture

---
+++ src/HughFor/Content/player.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="player" tilewidth="24" tileheight="24" tilecount="1" columns="1">
+ <image source="player.png" width="24" height="24"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="player"/>
+  </properties>
+ </tile>
+</tileset>
